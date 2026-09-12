13817246b306bd8d2871a33dd85dddc5dd4dd7f7
Mohd Abdul Ghani <gania828@gmail.com>
2026-09-12 23:02:56 +0800

    style(ui): sederhanakan tombol aksi pada tabel modul dan master

--- resources/views/system/sub-menus/index.blade.php
+++ resources/views/system/sub-menus/index.blade.php
@@ -115,7 +115,7 @@
                                     <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"></path>
                                     </svg>
-                                    <span>Edit</span>
+                                    <!-- <span>Edit</span> -->
                                 </a>
                                 <form method="POST" action="{{ route('system.sub-menus.destroy', $subMenu) }}" onsubmit="return confirm('Hapus sub-menu ini?');" class="form-inline-action">
                                     @csrf
@@ -125,7 +125,7 @@
                                             <polyline points="3 6 5 6 21 6"></polyline>
                                             <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
                                         </svg>
-                                        <span>Hapus</span>
+                                        <!-- <span>Hapus</span> -->
                                     </button>
                                 </form>
                             </div>
